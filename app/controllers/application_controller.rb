5371084b417dce111c2913fd2059e7035f0e68e0
John Cordero <johncorderox@gmail.com>
2018-04-04 00:50:38 +0000

    editing db user model

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,20 +3,4 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  def resource_name
-    :user
-  end
-
-  def resource
-    @resource ||= User.new
-  end
-
-  def resource_class
-    User
-  end
-
-  def devise_mapping
-    @devise_mapping ||= Devise.mappings[:user]
-  end
-  
 end
