92f7a90bc6ed29e4a62fed97e0ecf9a93b70d33f
John Cordero <johncorderox@gmail.com>
2018-04-12 03:04:21 +0000

    search function is done

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,9 @@
 class SearchController < ApplicationController
   def index
-    @search_users = User.joins(:moods).select("*").group("users.username")
+    if params[:search]
+      @search_users = User.where('username LIKE ? ', "%#{params[:search]}%").joins(:moods).select("*").group("users.username").order("users.id ASC")
+    else
+      @search_users = User.joins(:moods).select("*").group("users.username").order("users.id ASC")
+    end
   end
 end
